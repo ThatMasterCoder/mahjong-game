a4bc6f99ec10ba47f8b024d1051145bfeb08f891
ThatMasterCoder <youareanoob09@gmail.com>
2025-06-23 17:48:36 -0400

    added directories and distributed packages

---
+++ src/main/java/mahjong/Main.java
@@ -0,0 +1,12 @@
+package mahjong;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mahjong Game!");
+        // TODO: Implement game logic
+    }
+}
